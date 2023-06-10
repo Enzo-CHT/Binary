9ecfb421af80719f2c04192e85204103d19649ae
Enzo-CHT <134451832+Enzo-CHT@users.noreply.github.com>
2023-06-10 11:28:15 +1100

    v0.4.2
    Add 2 modes
    Modify display field
    The IP mode don't work because of the separators

--- web/js/script.js
+++ web/js/script.js
@@ -1,15 +1,18 @@
 
 
 
-
 function getBinaryNum(numLengh) {
     /* Generate a binary number */ 
 
     // Can only have the size of an octet or an IP address
     if (numLengh == 8 || numLengh == 32) {  
-        let bin = "";
+        let bin ="";
         for (let i=0; i<=numLengh-1 ; i++ ) {
             bin += Math.floor(Math.random()*2); // Generate 0 or 1
+            if (numLengh==32 && i%8 ==0 && i!=0){
+                // Add separators for IP address 
+                bin += ".";
+            }
         }
         return bin;
     }
@@ -31,27 +34,34 @@ function convertResult(bin) {
 function checkResult() {
     /* Check if the user input is right */
     let userAnswer = document.getElementById("guess-field").value ;
-    let actualBin = document.getElementById("display-field").value ;
+    
+    let actualBin = document.getElementById("display-field").innerHTML ;
+    
+
     
     // If user has the right answer he wins else he fails 
     if (userAnswer == convertResult(actualBin)) {
-        document.getElementById("statu").textContent = "Congrats!";
-        setTimeout(init,1000);
+        document.getElementById("statu").innerHTML = "Congrats!";
+        setTimeout(function() {
+            init(mode)
+        },1000);
 
     } else {
-        document.getElementById("statu").textContent = "Looser!";
-        setTimeout(init,1000);
+        document.getElementById("statu").innerHTML = "Looser!";
+        setTimeout(function() {
+            init(mode)
+        },1000);
     }
 
 }
 
 
-function init() {
+function init(num) {
+    mode = num;
     setTimeout('',500);
-    document.getElementById("statu").textContent = "";
-    document.getElementById("display-field").value = getBinaryNum(8);
+    document.getElementById("statu").innerHTML = "";
+    document.getElementById("display-field").innerHTML = getBinaryNum(num);
     getBinaryNum(8);
 }
 
-init();
-
+let mode = 0;
